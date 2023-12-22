c9149049a362b1c2cf78f6d1ea65d2047d25e69f
Michel Bolzon Souza dos Reis <michel.reis@>
2023-12-22 11:05:55 -0300

    Implement keyboard and mouse commands to execute loot

--- src/macros/auto_loot.py
+++ src/macros/auto_loot.py
@@ -1,3 +1,5 @@
+from pynput.keyboard import Key, Controller
+import pyautogui
 
 
 class AutoLoot:
@@ -7,4 +9,61 @@ class AutoLoot:
     
     def loot(self):
         if(self.player.justKillAMonster):
-          print('will Loot')
+          self.lootArroundPlayer()
+
+    def lootArroundPlayer(self):
+        (x,y) = pyautogui.size()
+        middleX = x/2
+        middleY = y/2
+
+        # Click Center
+        pyautogui.moveTo(middleX,middleY)
+        self.executeHotkeyLoot()
+        self.executeHotkeyLoot()
+
+        # Click Top Center
+        pyautogui.moveRel(0,50)
+        self.executeHotkeyLoot()
+        self.executeHotkeyLoot()
+
+        # Click Top Right
+        pyautogui.moveRel(50,0)
+        self.executeHotkeyLoot()
+        self.executeHotkeyLoot()
+
+        #Click Center Right
+        pyautogui.moveRel(0,-50)
+        self.executeHotkeyLoot()
+        self.executeHotkeyLoot()
+
+        #Click Down Right
+        pyautogui.moveRel(0,-50)
+        self.executeHotkeyLoot()
+        self.executeHotkeyLoot()
+
+        #Click Down Center
+        pyautogui.moveRel(-50,0)
+        self.executeHotkeyLoot()
+        self.executeHotkeyLoot()
+
+        #Click Down Left
+        pyautogui.moveRel(-50,0)
+        self.executeHotkeyLoot()
+        self.executeHotkeyLoot()
+
+        #Click Center Left
+        pyautogui.moveRel(0,-50)
+        self.executeHotkeyLoot()
+        self.executeHotkeyLoot()
+
+        #Click Top Left
+        pyautogui.moveRel(0,-50)
+        self.executeHotkeyLoot()
+        self.executeHotkeyLoot()
+
+
+    def executeHotkeyLoot():
+        keyboard = Controller()
+        keyboard.press(Key.shift)
+        pyautogui.rightClick()
+        keyboard.release(Key.shift)
